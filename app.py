7d57234b8987e626574daf1ebdcf15af1f56b11d
mzheng17-tech <mzheng17@terpmail.umd.edu>
2024-03-28 20:36:35 -0400

    displays an HTML form for users to input their information.

--- app.py
+++ app.py
@@ -2,17 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World from Maggie Zheng! I am adding my first code change.'
 
 @app.route('/hello')
 def hello():
     return render_template('hello.html')
-
-if __name__ == '__main__':
-    app.run()
 
 @app.route('/about')
 def about():
@@ -26,7 +22,6 @@ def about_css():
 def favorite_course():
     print('Subject entered: ' + request.args.get('subject'))
     print('Course number entered: ' + request.args.get('course_number'))
-
     return render_template('favorite-course.html')
 
 @app.route('/contact', methods=['GET', 'POST'])
@@ -36,10 +31,5 @@ def contact():
     else:
         return render_template('contact.html')
 
-
-
-
 if __name__ == '__main__':
     app.run()
-
-
