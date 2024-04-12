2e76d6c40154f074762249a9818caadfaab78b93
mzheng17-tech <mzheng17@terpmail.umd.edu>
2024-04-12 00:28:38 -0400

    updated naming of user table

--- models.py
+++ models.py
@@ -43,7 +43,7 @@ class Major(db.Model):
 
 
 class User(UserMixin, db.Model):
-    __tablename__ = "user"
+    __tablename__ = "users"
 
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
